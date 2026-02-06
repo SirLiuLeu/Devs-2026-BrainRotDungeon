4e1f2e36d011fc950f75d2b000beee8ea02a9f33
SirLiuLeu <vivandat0620@gmail.com>
2026-02-07 06:34:51 +0700

    Add placeholder asset models and map

---
+++ src/ReplicatedStorage/Shared/Config/AssetMap.lua
@@ -0,0 +1,13 @@
+local AssetMap = {
+    Monsters = {
+        Brainrot = "Brainrot",
+        EliteBrainrot = "EliteBrainrot",
+        BossBrainrot = "BossBrainrot",
+    },
+    Weapons = {
+        SwordA = "SwordA",
+        SwordB = "SwordB",
+    },
+}
+
+return AssetMap
