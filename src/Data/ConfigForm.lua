28daa7b3bea7ec4c98f272c6b7b45291f3d8c148
SirLiuLeu <vivandat0620@gmail.com>
2026-02-06 15:15:34 +0700

    Update data configs to align gameplay

--- src/Data/ConfigForm.lua
+++ src/Data/ConfigForm.lua
@@ -1,22 +1,67 @@
--- ReplicatedStorage/Config/Enemies
+-- Enemy configuration aligned with GamePlay.txt
 
-EnemyConfig = {
-    Id = "Brainrot",
+local EnemyConfig = {
+    Brainrot = {
+        Id = "Brainrot",
+        Type = "Normal",
 
-    Stats = {
-        HP = number,
-        Damage = number,
-        AttackRange = number,
-        Cooldown = number,
-        AttackType = "SingleTarget" | "AOE"
+        Stats = {
+            HP = 120,
+            Damage = 8,
+            AttackRange = 6,
+            Cooldown = 1,
+            AttackType = "SingleTarget",
+        },
+
+        Rewards = {
+            Exp = 20,
+            Gold = 25,
+            BoneChance = 0.05,
+            DropTable = "BrainrotBasic",
+        },
     },
 
-    Rewards = {
-        Exp = number,
-        Gold = number,
-        BoneChance = number,
-        DropTable = "BrainrotBasic"
-    }
+    EliteBrainrot = {
+        Id = "EliteBrainrot",
+        Type = "Elite",
+
+        Stats = {
+            HP = 450,
+            Damage = 22,
+            AttackRange = 8,
+            Cooldown = 1.2,
+            AttackType = "SingleTarget",
+        },
+
+        Rewards = {
+            Exp = 120,
+            Gold = 140,
+            BoneChance = 0.3,
+            DropTable = "BrainrotElite",
+        },
+    },
+
+    BossBrainrot = {
+        Id = "BossBrainrot",
+        Type = "Boss",
+
+        Stats = {
+            HP = 2500,
+            Damage = 40,
+            AttackRange = 12,
+            Cooldown = 2.5,
+            AttackType = "AOE",
+        },
+
+        Rewards = {
+            Exp = 700,
+            Gold = 900,
+            BoneChance = 1.0,
+            DropTable = "BrainrotBoss",
+            LastHitBonePercent = 0.3,
+            DamageContributionPercent = 0.7,
+        },
+    },
 }
 
-
+return EnemyConfig
---
+++ src/Data/DropTableConfig.lua
@@ -0,0 +1,50 @@
+local DropTableConfig = {
+    BrainrotBasic = {
+        Rolls = 1,
+        Items = {
+            { Item = "Bone", Chance = 0.05 },
+            { Item = "Potion_HP_Small", Chance = 0.02 },
+            { Item = "Sword_Common", Chance = 0.01 },
+        },
+        Pity = {
+            Enabled = true,
+            MaxStacks = 80,
+            BonusPerFail = 0.005,
+            GuaranteedAt = 80,
+        },
+    },
+
+    BrainrotElite = {
+        Rolls = 1,
+        Items = {
+            { Item = "Bone", Chance = 0.2 },
+            { Item = "Potion_HP_Medium", Chance = 0.08 },
+            { Item = "Sword_Uncommon", Chance = 0.03 },
+            { Item = "Armor_Rare", Chance = 0.02 },
+        },
+        Pity = {
+            Enabled = true,
+            MaxStacks = 60,
+            BonusPerFail = 0.01,
+            GuaranteedAt = 60,
+        },
+    },
+
+    BrainrotBoss = {
+        Rolls = 2,
+        Items = {
+            { Item = "Bone", Chance = 1.0 },
+            { Item = "Potion_Damage", Chance = 0.25 },
+            { Item = "Sword_Rare", Chance = 0.1 },
+            { Item = "Staff_Epic", Chance = 0.05 },
+        },
+        Pity = {
+            Enabled = true,
+            MaxStacks = 40,
+            BonusPerFail = 0.02,
+            GuaranteedAt = 40,
+        },
+    },
+}
+
+return DropTableConfig
