f1b64914e4f1ba8b3c88ff76733d35cf48340bb3
SirLiuLeu <vivandat0620@gmail.com>
2026-02-08 06:00:54 +0700

    a

--- src/ReplicatedStorage/Shared/Config/Skills.lua
+++ src/ReplicatedStorage/Shared/Config/Skills.lua
@@ -2,19 +2,19 @@ local Skills = {
     Basic = {
         DamagePercent = 1.0,
         Range = 20,
-        Cooldown = 1.5,
+        Cooldown = 0.2,
         Type = "Basic",
     },
     SkillE = {
         DamagePercent = 1.35,
         Range = 20,
-        Cooldown = 1.5,
+        Cooldown = 1,
         Type = "SkillE",
     },
     SkillR = {
         DamagePercent = 1.8,
         Range = 24,
-        Cooldown = 1.5,
+        Cooldown = 3.5,
         Type = "SkillR",
     },
 }
